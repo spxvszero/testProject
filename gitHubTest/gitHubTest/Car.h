e4064af8d9ad2e5b21bb7a9819b6f95345c2172c
jacky <jacky@jackydemacbook-pro.local>
2015-10-22 19:35:03 +0800

    创建了车类

---
+++ gitHubTest/gitHubTest/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  gitHubTest
+//
+//  Created by jacky on 15/10/22.
+//  Copyright © 2015年 jacky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ gitHubTest/gitHubTest/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  gitHubTest
+//
+//  Created by jacky on 15/10/22.
+//  Copyright © 2015年 jacky. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
