30296bb0ea56a951d19542218c094587e6264811
jacky <jacky@jackydemacbook-pro.local>
2015-10-22 19:00:23 +0800

    初始化项目

---
+++ gitHubTest/gitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHubTest
+//
+//  Created by jacky on 15/10/22.
+//  Copyright © 2015年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHubTest/gitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubTest
+//
+//  Created by jacky on 15/10/22.
+//  Copyright © 2015年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
